beb70cee5778943fdcb147a877593f9495ee3993
fkolano <fkolano@gmail.com>
2022-06-16 00:53:09 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,9 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+function SimpleCounter(props) {
+  return <p>Hello World</p>;
+}
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
